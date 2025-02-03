57218b62911fe0a43e65ff1ebd9b11b183765207
Rahul Kumar <r0k067s@homeoffice.wal-mart.com>
2025-02-03 12:04:56 +0530

    add comment

--- rectangle.py
+++ rectangle.py
@@ -4,11 +4,14 @@ def perimeter(length, breath):
 def area(length, breath):
     return length * breath
 
-len = 10
-b = 4
+len = int(input('Enter Length : '))
+b = int(input('Enter Breadth : '))
+ # named parameter 
+ # I can call like perimeter(breath = b, length = len) as well
+p = perimeter(length = len, breath = b)
 
-p = perimeter(len,b)
+# unnamed parameter
 a = area(len, b)
 
-print('The perimeter is : ', p)
-print(' The area of rectangle is : ', a)
+print('The perimeter is : ', str(p))
+print(' The area of rectangle is : ', str(a))
